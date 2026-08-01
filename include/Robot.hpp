d4e7382728c7fc909bdf272524c0575b8b5b17ab
The-codeMachine <coolestbro5113@gmail.com>
2026-08-01 17:10:54 -0600

    Nearly done serialization
    To complete serialization I need to make a Device factory, then test file loading/saving

--- include/Robot.hpp
+++ include/Robot.hpp
@@ -27,6 +27,9 @@ class Robot : public Object {
 public:
     Robot(World& world, Transform t);
 
+    void deserialize(const nlohmann::json& json) override;
+    nlohmann::json serialize() const override;
+
     /// @brief Registers the robot to Objects. 
     static void registerRobot();
 
--- src/robot.cpp
+++ src/robot.cpp
@@ -2,6 +2,14 @@
 
 Robot::Robot(World& world, Transform t) : Object(world, t, "Robot") {}
 
+void Robot::deserialize(const nlohmann::json& json) {
+
+}
+
+nlohmann::json Robot::serialize() const {
+    return {};
+}
+
 void Robot::registerRobot() {
     Object::Object_Factory.registerType<Robot>("Robot");
 }
