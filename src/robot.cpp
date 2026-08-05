bd1617bed670fcc49ef45a004483ff907826e54c
The-codeMachine <coolestbro5113@gmail.com>
2026-08-05 09:38:46 -0600

    Made some fixes and added to the workflows fixing some more issues

--- src/robot.cpp
+++ src/robot.cpp
@@ -10,7 +10,7 @@ void Robot::deserialize(const nlohmann::json& json) {
     devices_.clear();
 
     for (const auto& j : json.at("data").at("devices")) {
-        auto device = Device::Device_Factory.create(j.at("type"), j.at("id"));
+        auto device = Device::Device_Factory.create(std::string_view(j.at("type")), j.at("id"));
         device->deserialize(j);
 
         addDevice<Device>(std::move(device));
